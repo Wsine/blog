9f94c949cbabe232e68898df056ac67cc440ceb0
Wsine <weizhy3@hotmail.com>
2020-12-05 20:19:04 +0800

    update css style

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -105,11 +105,6 @@ query Post ($id: ID!) {
       margin-top: 0;
     }
 
-    p:first-of-type {
-      font-size: 1.2em;
-      color: var(--title-color);
-    }
-
     img {
       width: calc(100% + var(--space) * 2);
       margin-left: calc(var(--space) * -1);
